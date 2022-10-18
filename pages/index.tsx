3a0ceeb0ddfd6959b119fd58675893ea07dd1169
José Alves <jose.alves@25friday.com>
2022-10-18 22:11:23 +0100

    feed profile page with real data; started top artist UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,55 @@
-import Head from "next/head";
-import Image from "next/image";
+import { Box, Flex, Text } from "@chakra-ui/layout";
+import { Image } from "@chakra-ui/react";
 import GradientLayout from "../components/gradientLayout";
-import styles from "../styles/Home.module.css";
+import { useMe } from "../lib/hooks";
+import prisma from "../lib/prisma";
 
-export default function Home() {
+const Home = ({ artists }) => {
+  const { user } = useMe();
+
   return (
     <GradientLayout
-      color="green"
+      color="purple"
       subtitle="profile"
-      title="José Alves"
-      description="10 public playlists"
+      title={`${user?.firstName} ${user?.lastName}`}
+      description={`${user?.playlistCount} public playlists`}
       image="https://i.pinimg.com/originals/62/1a/fc/621afc7f8901bb3aa2625bd8a744d16c.jpg"
       roundImage
     >
-      <div>Home page</div>
+      <Box paddingX="40px" color="white">
+        <Box marginBottom="40px">
+          <Text fontSize="2xl" fontWeight="bold">
+            Top artist this month
+          </Text>
+          <Text fontSize="md">only visible to you</Text>
+        </Box>
+        <Flex>
+          {artists.map((artist) => (
+            <Box paddingX="10px" width="20%">
+              <Box bg="gray.900" borderRadius="4px" padding="15px" width="100%">
+                <Image
+                  src="https://img.freepik.com/vetores-premium/logotipo-com-emblema-de-cao-beagle-fofo-isolado-no-branco_295412-97.jpg?w=2000"
+                  borderRadius="100%"
+                />
+                <Box marginTop="20px">
+                  <Text fontSize="large">{artist.name}</Text>
+                  <Text fontSize="x-small">Artist</Text>
+                </Box>
+              </Box>
+            </Box>
+          ))}
+        </Flex>
+      </Box>
     </GradientLayout>
   );
-}
+};
+
+export const getServerSideProps = async () => {
+  const artists = await prisma.artist.findMany({});
+
+  return {
+    props: { artists },
+  };
+};
+
+export default Home;
