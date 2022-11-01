5915c0c699c6c8217b37908302d24860f460145a
José Alves <jose.alves@25friday.com>
2022-11-01 12:35:51 +0000

    playlist songs

---
+++ components/songsTable.tsx
@@ -0,0 +1,57 @@
+import { Box } from "@chakra-ui/layout";
+import { Table, Thead, Td, Tr, Tbody, IconButton, Th } from "@chakra-ui/react";
+import { BsFillPlayFill } from "react-icons/bs";
+import { AiOutlineClockCircle } from "react-icons/ai";
+import { formatDate, formatTime } from "../lib/formatters";
+
+const SongTable = ({ songs }) => {
+  return (
+    <Box bg="transparent" color="white">
+      <Box padding="10px" marginBottom="20px">
+        <Box marginBottom="30px">
+          <IconButton
+            icon={<BsFillPlayFill />}
+            fontSize="30px"
+            colorScheme="green"
+            size="lg"
+            isRound
+            aria-label="play"
+          />
+        </Box>
+        <Table variant="unstyled">
+          <Thead borderBottom="1px solid" borderColor="rgb(255,255,255,0.2)">
+            <Tr>
+              <Th>#</Th>
+              <Th>Title</Th>
+              <Th>Date added</Th>
+              <Th>
+                <AiOutlineClockCircle />
+              </Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {songs.map((song, i) => (
+              <Tr
+                key={song.id}
+                cursor="cursor"
+                sx={{
+                  transition: "all .3s ",
+                  "&:hover": {
+                    bg: "rgba(255,255,255,0.1)",
+                  },
+                }}
+              >
+                <Td>{i + 1}</Td>
+                <Td>{song.name}</Td>
+                <Td>{formatDate(song.createdAt)}</Td>
+                <Td>{formatTime(song.duration)}</Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </Box>
+    </Box>
+  );
+};
+
+export default SongTable;
--- lib/auth.ts
+++ lib/auth.ts
@@ -28,3 +28,8 @@ export const validateRoute = (handler) => {
     res.json({ error: "Not authorised" });
   };
 };
+
+export const validateToken = (token) => {
+  const user = jwt.verify(token, "hello");
+  return user;
+};
